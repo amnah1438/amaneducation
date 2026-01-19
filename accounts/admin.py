f019eb2c9d3208a62da88a0152a0f08555884bd0
amnah1438 <__@domain.com>
2026-01-20 02:14:38 +0300

    تعديل

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from .models import Profile
 
-# هنا نترك فقط إدارة ملفات المستخدمين الخاصة بتطبيق accounts
-# ونحذف أي تسجيل لـ Skill أو Teacher لأنه موجود في تطبيق core
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "role", "pin_code")
-    list_filter = ("role",)
+    list_display = ("user", "role")
