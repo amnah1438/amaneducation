c54b444edfb88d76624e05f77f30527e77eb99ec
amnah1438 <__@domain.com>
2026-01-23 00:01:16 +0300

    تعديل الو آر اس

--- core/urls.py
+++ core/urls.py
@@ -4,21 +4,23 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
+# ضعيه هنا خارج المصفوفة وليس داخل path
+admin.site.site_header = "إدارة منصة آمنة التعليمية"
+
 urlpatterns = [
-    path('admin/', admin.site.site_header), # مسار لوحة الإدارة
+    # تأكدي أن السطر مكتوب هكذا فقط:
     path('admin/', admin.site.urls),
     
-    # مسارات المنصة الأساسية
+    # مسارات المنصة
     path("", views.home, name="home"),
     path("skill/<int:skill_id>/", views.skill_detail, name="skill_detail"),
     path("skill/<int:skill_id>/test/", views.take_test, name="take_test"),
 
-    # --- رابط مكتبة رفع الصور (ضروري جداً لعمل المحرر) ---
+    # رابط رفع الصور
     path('ckeditor/', include('ckeditor_uploader.urls')),
-
 ]
 
-# --- إعدادات عرض الصور والملفات المرفوعة أثناء التطوير ---
+# إعدادات ملفات الميديا
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
