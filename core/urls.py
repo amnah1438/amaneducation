9c09ad3fb0052de43bf9ea827a1c7cb2a81df6a7
amnah1438 <__@domain.com>
2026-01-22 23:57:38 +0300

    تعديل اليو آر اس

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,24 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    # مسار الصفحة الرئيسية
-    path("", views.home, name="home"),
+    path('admin/', admin.site.site_header), # مسار لوحة الإدارة
+    path('admin/', admin.site.urls),
     
-    # مسار صفحة الدرس/المهارة
+    # مسارات المنصة الأساسية
+    path("", views.home, name="home"),
     path("skill/<int:skill_id>/", views.skill_detail, name="skill_detail"),
-
-    # المسار الجديد الذي أضفته لكِ لفتح صفحة الاختبار
     path("skill/<int:skill_id>/test/", views.take_test, name="take_test"),
+
+    # --- رابط مكتبة رفع الصور (ضروري جداً لعمل المحرر) ---
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+
 ]
+
+# --- إعدادات عرض الصور والملفات المرفوعة أثناء التطوير ---
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
