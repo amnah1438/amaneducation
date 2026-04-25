869a4ce37a56692e71e92ccefda556fc634363f4
اسمك هنا < >
2026-04-25 10:40:00 +0300

    تعديل

--- students/urls.py
+++ students/urls.py
@@ -6,3 +6,4 @@ urlpatterns = [
     path('exam/<int:exam_id>/', views.take_exam, name='take_exam'),
     path('result/<int:result_id>/', views.student_result_view, name='student_result_view'),
 ]
+path('import/', views.import_students_excel, name='import_students_excel'),
