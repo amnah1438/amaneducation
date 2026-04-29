a81751c52ffd5eb9b54533d6eccfba30ddfdbe9b
اسمك هنا < >
2026-04-29 07:56:09 +0300

    juvdg

--- teachers/views.py
+++ teachers/views.py
@@ -1,4 +1,3 @@
-Content is user-generated and unverified.
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
