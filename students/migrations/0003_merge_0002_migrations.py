ca23590cf4021f1820e63361e96ad63c1266c7d1
Amnah <moonoy1438@gmail.com>
2026-08-26 08:10:58 +0300

    fix: merge conflicting migrations

---
+++ students/migrations/0003_merge_0002_migrations.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('students', '0002_alter_classroom_options_alter_student_options_and_more'),
+        ('students', '0002_remedialexamassignment'),
+    ]
+
+    operations = [
+    ]
